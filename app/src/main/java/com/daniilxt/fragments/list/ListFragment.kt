da2aeb4d33ffde758df92913611a6eb157d3f433
daniilxt <daniil-firsov@mail.ru>
2021-02-18 22:23:45 +0300

    added list frg menu

--- app/src/main/java/com/daniilxt/fragments/list/ListFragment.kt
+++ app/src/main/java/com/daniilxt/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.daniilxt.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.daniilxt.todo_app.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -16,13 +14,23 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
-        view.findViewById<FloatingActionButton>(R.id.list_frg__fab).setOnClickListener{
-        findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        //used that, while dint know view binding
+        view.findViewById<FloatingActionButton>(R.id.list_frg__fab).setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
+        view.findViewById<View>(R.id.list_frg__layout).setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+        setHasOptionsMenu(true)
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_frg__menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
